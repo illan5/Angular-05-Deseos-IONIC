a3baf2f82817a395b9a6166e1a2f8da9776f24cb
Jose Illan <jose_illan@hotmail.com>
2020-04-02 21:12:27 +0200

    Created Screens and buttons

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,47 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  
+  constructor( public deseosService: DeseosService, 
+               private router: Router,
+               private alterCtrl: AlertController ){  }
+
+  async agregarLista() {
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+
+    const alert = await this.alterCtrl.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: ( data ) => {
+            console.log(data);
+            if ( data.titulo.length === 0 ){
+              return;
+            }
+            this.deseosService.crearLista( data.titulo );
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+
+  }
 
 }
